1c0a2fa16e5627ce7bd7b956972b2304ed04e6ac
ima1zumi <mariimaizumi5@gmail.com>
2023-02-11 23:51:38 +0900

    First commit

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  attr_reader :name
+  attr_reader :contacts
+
+  def initialize(name:)
+    @name = name
+    @contacts = []
+  end
+
+  def guess_country
+    contacts.map do |contact|
+      case contact
+      when Phone
+        contact.country
+      end
+    end.compact.first
+  end
+end
